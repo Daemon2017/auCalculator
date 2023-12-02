57617ba0a0670520f0bba28d74028a9a5e3ae350
Daemon2017 <daemon2017@gmail.com>
2023-12-02 21:49:29 +0300

    Переработан код

--- back/run.py
+++ back/run.py
@@ -14,19 +14,22 @@ cors = CORS(app)
 
 @app.route('/subtract', methods=['POST'])
 def request_txt():
-    percent = abs(float(request.headers['percent'])) / 100
     json_data = json.loads(request.data)
-    component_io = StringIO(json_data['component'])
-    component_df = pd.read_csv(component_io, sep=",", header=None)
-    samples_io = StringIO(json_data['samples'])
-    samples_df = pd.read_csv(samples_io, sep=",", header=None)
-    component_df = component_df.drop([0], axis=1)
-    samples_names_df = pd.DataFrame(samples_df[0])
+    component_names_df, component_data_df = get_names_data_df(json_data['component'])
+    samples_names_df, samples_data_df = get_names_data_df(json_data['samples'])
     samples_names_df[0] = samples_names_df[0].str.replace(r'^([^:]+)', r'\1_sub', regex=True)
-    samples_df = samples_df.drop([0], axis=1)
-    calculated_df = pd.DataFrame((samples_df.values - percent * component_df.values[0]) / (1 - percent))
-    result_df = pd.concat([samples_names_df, calculated_df], axis=1)
+    percent = abs(float(request.headers['percent'])) / 100
+    calculated_data_df = pd.DataFrame((samples_data_df.values - percent * component_data_df.values[0]) / (1 - percent))
+    result_df = pd.concat([samples_names_df, calculated_data_df], axis=1)
     return Response(result_df.to_csv(sep=',', index=False, header=False), mimetype=TEXT_CSV)
+
+
+def get_names_data_df(data):
+    data_io = StringIO(data)
+    data_df = pd.read_csv(data_io, sep=",", header=None)
+    names_df = pd.DataFrame(data_df[0])
+    data_df = data_df.drop([0], axis=1)
+    return names_df, data_df
 
 
 if __name__ == '__main__':
